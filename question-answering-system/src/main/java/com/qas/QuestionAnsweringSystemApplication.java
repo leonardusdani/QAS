9b51bb95606454c13139d49eddc74e5f7e5ae286
Leonardus Dani Novianto <leonardus.dani@gmail.com>
2016-03-25 23:25:55 +0700

    Reconfigure project structure

---
+++ question-answering-system/src/main/java/com/qas/QuestionAnsweringSystemApplication.java
@@ -0,0 +1,12 @@
+package com.qas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionAnsweringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionAnsweringSystemApplication.class, args);
+	}
+}
